d148b09fd5b1d13082c1e62f56993fbb7beccb3e
CrimpyCode <wsung88@gmail.com>
2022-11-23 21:08:03 -0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,97 +1,96 @@
-import { useState } from 'react';
-import axios from 'axios';
-import useQuery from './useQuery';
-
-
+import { useState } from "react";
+import axios from "axios";
+import useQuery from "./useQuery";
 
 //URL used for Get and Post request
-const url = "https://frontend-take-home.fetchrewards.com/form"
+const url = "https://frontend-take-home.fetchrewards.com/form";
 
-const inputClassName = "mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+const inputClassName =
+  "mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm";
 
 const emptyForm = {
-  name: '',
-  email: '',
-  password: '',
-  occupation: '',
-  state: '',
+  name: "",
+  email: "",
+  password: "",
+  occupation: "",
+  state: "",
 };
-  
-const makeInputLabel = (name) =>
-name[0].toUpperCase() + name.substring(1) + ': ';
 
+const makeInputLabel = (name) =>
+  name[0].toUpperCase() + name.substring(1) + ": ";
 
 const TextInput = ({ name, placeholder, password, value, onChange }) => (
-  <>
+  <div className="col-span-6 sm:col-span-3">
     {makeInputLabel(name)}
     <input
       required
-      type={password ? 'password' : 'text'}
+      type={password ? "password" : "text"}
       name={name}
       value={value}
       placeholder={placeholder}
       onChange={onChange}
       className={inputClassName}
     />
-  </>
+  </div>
 );
-const SelectInput = ({ name, value, onChange }) => (
-  <>
-    {makeInputLabel(name)}
+
+const SelectInput = ({ name, value, onChange, options }) => (
+  <div className="col-span-6 sm:col-span-3">
+  {makeInputLabel(name)}
     <select
       name={name}
       onChange={onChange}
       className={inputClassName}
       value={value}
+      required
     >
-      {(name === "occupation") ? occupationsOptions : statesOptions}
+      <>
+      <option selected disabled value={''} >Select...</option>
+      {options.map(({ name }) => (
+        <option value={name} key={name}>
+          {name}
+        </option>
+      ))}
+      </>
     </select>
-  </>
+  </div>
 );
 
-
-const statesOptions = states.map(({ name, abbreviation }) => (
-  <option value={name} key={abbreviation}>
-    {name}
-  </option>
-));
-
-const occupationsOptions = occupations.map((value, i) => (
-  <option value={value} key={i}>
-    {value}
-  </option>
-));
-
-
 function App() {
+
+  const [success, setSuccess] = useState(false)
   
   const { occupations, states } = useQuery();
 
+  const occupationOptions = occupations.map((occupation) => ({
+    name: occupation,
+  }));
+  
   //setting up a template for all of the input values from the form
-  const [formValue, setFormValue] = useState(emptyForm)
+  const [formValue, setFormValue] = useState(emptyForm);
 
   // event handling for text inputs which are name, email and password
   const handleChange = ({ target: { name, value } }) => {
     setFormValue({
       ...formValue,
-      [name]: value
+      [name]: value,
     });
-  }
+  };
 
   //event handling for submitting Post request from form
   const handleSubmit = (event) => {
-    axios.post((url), formValue).then(res => {
+    axios.post(url, formValue).then((res) => {
       if (res.status === 201) {
-        setFormValue(emptyForm)
-        alert("You have successfully created a user!");
+        setFormValue(emptyForm);
+        setSuccess(true)
+        // alert("You have successfully created a user!");
       }
-    })
+    });
     event.preventDefault();
-  }
+  };
 
   return (
     <div className="App">
-
       <form action="#" method="POST" onSubmit={handleSubmit}>
         <div className="overflow-hidden shadow sm:rounded-md">
           <div className="bg-white px-4 py-5 sm:p-6">
@@ -101,7 +100,7 @@ function App() {
                   name="name"
                   value={formValue.name}
                   onChange={handleChange}
-                  placeholder='John Smith'
+                  placeholder="John Smith"
                 />
               </div>
 
@@ -110,7 +109,7 @@ function App() {
                   name="email"
                   value={formValue.email}
                   onChange={handleChange}
-                  placeholder='example@email.com'
+                  placeholder="example@email.com"
                 />
               </div>
 
@@ -119,7 +118,8 @@ function App() {
                   name="password"
                   value={formValue.password}
                   onChange={handleChange}
-                  placeholder='•••••••'
+                  placeholder="•••••••"
+                  password
                 />
               </div>
 
@@ -127,6 +127,7 @@ function App() {
                 <SelectInput
                   name="occupation"
                   value={formValue.occupation}
+                  options={occupationOptions}
                   onChange={handleChange}
                   className={inputClassName}
                 />
@@ -136,11 +137,11 @@ function App() {
                 <SelectInput
                   name="state"
                   value={formValue.state}
+                  options={states}
                   onChange={handleChange}
                   className={inputClassName}
                 />
               </div>
-
             </div>
           </div>
           <div className="bg-gray-50 px-4 py-3 text-left sm:px-6">
@@ -150,6 +151,7 @@ function App() {
             >
               Submit
             </button>
+            {success && <div>Successfully submitted.</div>}
           </div>
         </div>
       </form>
